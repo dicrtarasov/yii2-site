f51ac04dcbfd6541d032e8f5d884e30a4d2a44c8
Igor A Tarasov <develop@dicr.org>
2020-08-16 04:13:26 +0500

    delete extra methods from Checkout/Order interface

--- src/order/CheckoutInterface.php
+++ src/order/CheckoutInterface.php
@@ -3,13 +3,11 @@
  * @copyright 2019-2020 Dicr http://dicr.org
  * @author Igor A Tarasov <develop@dicr.org>
  * @license proprietary
- * @version 16.08.20 02:05:58
+ * @version 16.08.20 04:13:01
  */
 
 declare(strict_types = 1);
 namespace dicr\site\order;
-
-use yii\base\Model;
 
 /**
  * Интерфейс формы оформления заказа.
@@ -17,38 +15,9 @@ use yii\base\Model;
 interface CheckoutInterface
 {
     /**
-     * Возвращает товары корзины.
-     *
-     * @return array товары
-     */
-    public function getProds(): array;
-
-    /**
      * Возвращает сумму заказа.
      *
      * @return float
      */
     public function getSum(): float;
-
-    /**
-     * Форма контактов покупателя
-     *
-     * @return Model
-     * Конкретный тип return переопределяется в реализации
-     */
-    public function getOrderContacts();
-
-    /**
-     * Выбранный метод оплаты.
-     *
-     * @return ?PayMethod
-     */
-    public function getPayMethod(): ?PayMethod;
-
-    /**
-     * Выбранный метод доставки.
-     *
-     * @return ?ShipMethod
-     */
-    public function getShipMethod(): ?ShipMethod;
 }
--- src/order/OrderInterface.php
+++ src/order/OrderInterface.php
@@ -3,13 +3,11 @@
  * @copyright 2019-2020 Dicr http://dicr.org
  * @author Igor A Tarasov <develop@dicr.org>
  * @license proprietary
- * @version 16.08.20 02:07:08
+ * @version 16.08.20 04:12:19
  */
 
 declare(strict_types = 1);
 namespace dicr\site\order;
-
-use yii\base\Model;
 
 /**
  * Интерфейс заказа.
@@ -17,38 +15,9 @@ use yii\base\Model;
 interface OrderInterface
 {
     /**
-     * Товары.
-     *
-     * @return array
-     */
-    public function getProds(): array;
-
-    /**
      * Сумма товаров.
      *
      * @return float
      */
     public function getSum(): float;
-
-    /**
-     * Модель контактов.
-     *
-     * @return Model
-     * Конкретный тип return переопределяется в реализации.
-     */
-    public function getOrderContacts();
-
-    /**
-     * Способ оплаты.
-     *
-     * @return ?PayMethod
-     */
-    public function getPayMethod(): ?PayMethod;
-
-    /**
-     * Способ доставки.
-     *
-     * @return ?ShipMethod
-     */
-    public function getShipMethod(): ?ShipMethod;
 }
