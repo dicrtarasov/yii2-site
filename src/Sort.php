98f380f76cc7103ecc348cbed25d5d82e326c106
Игорь Тарасов <develop@dicr.org>
2021-07-25 02:19:01 +0500

    fix asc/desc

--- src/Sort.php
+++ src/Sort.php
@@ -3,15 +3,19 @@
  * @copyright 2019-2021 Dicr http://dicr.org
  * @author Igor A Tarasov <develop@dicr.org>
  * @license proprietary
- * @version 06.01.21 05:21:51
+ * @version 25.07.21 02:16:30
  */
 
 declare(strict_types = 1);
 
 namespace dicr\site;
 
+use Yii;
+use yii\web\Request;
+
 use function array_key_first;
-use function count;
+use function mb_substr;
+use function reset;
 
 use const SORT_ASC;
 use const SORT_DESC;
@@ -19,11 +23,70 @@ use const SORT_DESC;
 /**
  * Class Sort
  *
- * @property-read string $sort текущее значение параметра сортировки
+ * @property ?string $sort текущее значение сортировки (один параметр)
+ * @property ?string $defaultSort сортировка по-умолчанию (одним параметром)
  * @property-read bool $isDefault является ли сортировка по-умолчанию
  */
 class Sort extends \yii\data\Sort
 {
+    /** @var string|false */
+    private $_defaultSort;
+
+    /**
+     * Сортировка по-умолчанию.
+     * (в отличии от defaultOrder поддерживает только одну сортировку
+     *
+     * @return ?string название сортировки по-умолчанию или null, если не задано.
+     * При сортировке по-убыванию добавляется "-"
+     */
+    public function getDefaultSort(): ?string
+    {
+        if ($this->_defaultSort === null) {
+            $defaultOrder = $this->defaultOrder;
+            if (! empty($defaultOrder)) {
+                $defaultSort = array_key_first($defaultOrder);
+                if ($defaultOrder[$defaultSort] === SORT_DESC) {
+                    $defaultSort = '-' . $defaultSort;
+                }
+
+                $this->_defaultSort = $defaultSort;
+            }
+
+            if ($this->_defaultSort === null) {
+                $this->_defaultSort = false;
+            }
+        }
+
+        return $this->_defaultSort ?: null;
+    }
+
+    /**
+     * Устанавливает сортировку по-умолчанию.
+     *
+     * @param string $sort название сортировки (для по-убыванию начинается с "-")
+     * @return $this
+     */
+    public function setDefaultSort(string $sort): Sort
+    {
+        $this->_defaultSort = $sort;
+
+        // также изменяем defaultOrder
+        $order = SORT_ASC;
+        if ($sort[0] === '-') {
+            $order = SORT_DESC;
+            $sort = mb_substr($sort, 1);
+        }
+
+        $this->defaultOrder = [
+            $sort => $order
+        ];
+
+        return $this;
+    }
+
+    /** @var string|false */
+    private $_sort;
+
     /**
      * Возвращает текущее значение параметра сортировки.
      *
@@ -32,32 +95,44 @@ class Sort extends \yii\data\Sort
      */
     public function getSort(bool $emptyIfDefault = false): ?string
     {
-        $current = $this->attributeOrders;
-        if (empty($current)) {
-            return null;
-        }
-
-        $currentSort = array_key_first($current);
-        if (($current[$currentSort] ?? SORT_ASC) === SORT_DESC) {
-            $currentSort = '-' . $currentSort;
-        }
-
-        if ($emptyIfDefault) {
-            $default = $this->defaultOrder;
-
-            if (count($default) === 1) {
-                $defaultSort = array_key_first($default);
-                if (($default[$defaultSort] ?? SORT_ASC) === SORT_DESC) {
-                    $defaultSort = '-' . $defaultSort;
+        if ($this->_sort === null) {
+            if (! empty($this->sortParam)) {
+                $params = $this->params;
+                if ($params === null) {
+                    $params = Yii::$app->request instanceof Request ? Yii::$app->request->queryParams : [];
                 }
 
-                if ($currentSort === $defaultSort) {
-                    $currentSort = null;
+                if (! empty($params[$this->sortParam])) {
+                    $currentSort = $this->parseSortParam($params[$this->sortParam]);
+                    if (! empty($currentSort)) {
+                        $this->_sort = reset($currentSort);
+                    }
                 }
+            }
+
+            if (empty($this->_sort)) {
+                $this->_sort = false;
             }
         }
 
-        return $currentSort;
+        return empty($this->_sort) || ($emptyIfDefault && $this->_sort === $this->defaultSort) ? null : $this->_sort;
+    }
+
+    /**
+     * Устанавливает текущую сортировку.
+     *
+     * @param ?string $sort
+     * @return $this
+     */
+    public function setSort(?string $sort): Sort
+    {
+        $this->_sort = $sort ?? false;
+
+        $this->params = [
+            $this->sortParam => $sort
+        ];
+
+        return $this;
     }
 
     /**
@@ -67,7 +142,7 @@ class Sort extends \yii\data\Sort
      */
     public function getIsDefault(): bool
     {
-        return $this->getSort(true) === null;
+        return $this->sort === $this->defaultSort;
     }
 
     /**
