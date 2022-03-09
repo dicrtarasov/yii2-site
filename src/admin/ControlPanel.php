95ecb69fd57445e7dc6c34500d536d4c9d4aa901
Игорь Тарасов <develop@dicr.org>
2022-03-10 00:16:19 +0300

    fix ControlPanel

--- src/admin/ControlPanel.php
+++ src/admin/ControlPanel.php
@@ -3,7 +3,7 @@
  * @copyright 2019-2022 Dicr http://dicr.org
  * @author Igor A Tarasov <develop@dicr.org>
  * @license proprietary
- * @version 05.01.22 21:05:06
+ * @version 10.03.22 00:12:22
  */
 
 declare(strict_types = 1);
@@ -32,7 +32,7 @@ class ControlPanel extends Widget
     public string|array|null $download = null;
 
     /** @var string[] дополнительные кнопки */
-    public array $buttons = [];
+    public ?array $buttons = null;
 
     /**
      * @inheritDoc
